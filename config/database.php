4cc611761d59a3d591344b2dff43d1bd0561c59a
soregashi-27 <tomo.soul.15@gmail.com>
2021-08-20 18:38:31 +0900

    パスワードの変更

--- config/database.php
+++ config/database.php
@@ -50,7 +50,7 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forum'),
             'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', 'forum'),
+            'password' => env('DB_PASSWORD', 'root'),
             'unix_socket'   => '/Applications/MAMP/tmp/mysql/mysql.sock',
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
