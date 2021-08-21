d251e4e61029e345dc54d687d6d8fecf07a6377e
soregashi-27 <tomo.soul.15@gmail.com>
2021-08-21 15:55:57 +0900

    編集画面の追加

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,5 @@ Route::get('/posts/create', 'PostController@create')->name('posts.create');
 Route::post('/posts', 'PostController@store')->name('posts.store');
 
 Route::get('/posts/{post}', 'PostController@show')->name('posts.show');
+
+Route::get('/posts/{post}/edit', 'PostController@edit')->name('posts.edit');
