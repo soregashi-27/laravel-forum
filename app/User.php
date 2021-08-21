2bed5aad3975c81cdab38ffe7c07d2c93e3c052d
soregashi-27 <tomo.soul.15@gmail.com>
2021-08-21 18:13:01 +0900

    投稿内容にユーザーを追加

--- app/User.php
+++ app/User.php
@@ -35,4 +35,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function posts()
+    {
+        return $this->hasMany('App\Post');
+    }
+
+    public function comments()
+    {
+        return $this->hasMany('App\Comment');
+    }
 }
